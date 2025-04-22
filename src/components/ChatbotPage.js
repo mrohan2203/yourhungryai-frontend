d779f6bed135fe572e5ebc940b78854f4064a050
Rohan M <rohan.muru2203@gmail.com>
2025-04-22 01:27:20 -0500

    Your commit message

--- src/components/ChatbotPage.js
+++ src/components/ChatbotPage.js
@@ -162,13 +162,14 @@ const ChatbotPage = () => {
   const generateRecipeImage = async (dishName) => {
     try {
       setIsGeneratingImage(true);
+      const query = `${dishName} in a bowl food photography`;
       const response = await axios.get(
-        `https://api.unsplash.com/search/photos?query=${encodeURIComponent(dishName)}&client_id=${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}&per_page=1&orientation=landscape`
+        `https://api.unsplash.com/search/photos?query=${encodeURIComponent(query)}&client_id=${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}&per_page=1&orientation=landscape`
       );
       if (response.data.results.length > 0) {
         return {
           url: response.data.results[0].urls.regular,
-          alt: dishName
+          alt: `${dishName} in a bowl`
         };
       }
       return null;
